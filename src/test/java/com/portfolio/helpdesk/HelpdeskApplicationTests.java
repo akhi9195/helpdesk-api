f5ce36a998573b2e4dfa2d796ec8095d8f956249
akhi9195 <akhi9195@gmail.com>
2026-09-24 17:58:39 +0530

    DB setup with flyware is done

--- src/test/java/com/portfolio/helpdesk/HelpdeskApplicationTests.java
+++ src/test/java/com/portfolio/helpdesk/HelpdeskApplicationTests.java
@@ -1,13 +1,28 @@
 package com.portfolio.helpdesk;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootTest
 class HelpdeskApiApplicationTests {
 
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void flywayAppliedBaselineMigration() {
+		Integer applied = jdbcTemplate.queryForObject(
+				"SELECT COUNT(*) FROM flyway_schema_history WHERE version = '1' AND success",
+				Integer.class);
+		assertThat(applied).isEqualTo(1);
 	}
 
 }
